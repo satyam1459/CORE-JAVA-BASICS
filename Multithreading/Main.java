fcc4eea8f1cede1c99ed8380701e56a1ca358b90
Satyam <Satyamjaiswal08795@gmail.com>
2023-03-29 15:53:31 +0530

    Changes

--- Multithreading/Main.java
+++ Multithreading/Main.java
@@ -1,7 +1,8 @@
 package Multithreading;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         System.out.println("Ya allah");
+        System.out.println("Samindar");
     }
 }
